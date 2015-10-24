1bcf90a7873eeb626b5e06cbdf7436f98f2e9fa2
Supermakaka <Supermakaka@inbox.lv>
2015-10-24 16:41:48 +0300

    Added EmailService.cs class

---
+++ BoilerPlate/BusinessLogicDatabaseFirst/Services/EmailService.cs
@@ -0,0 +1,22 @@
+﻿using BusinessLogic.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic.Services
+{
+    public class EmailService : BaseService<User>, IEmailService
+    {
+        public EmailService(DataContext dataContext) : base(dataContext)
+        {
+
+        }
+    }
+
+    public interface IEmailService : IService<User>
+    {
+
+    }
+}
